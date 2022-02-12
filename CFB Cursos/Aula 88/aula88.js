2423dfbc19ad119087d6fb259f34584b95c3117d
AdemirRamos <ademirramos634@gmail.com>
2022-02-12 12:20:56 -0300

    Atualização da Aula 88

--- CFB Cursos/Aula 88/aula88.js
+++ CFB Cursos/Aula 88/aula88.js
@@ -7,7 +7,7 @@ const elementos = document.querySelectorAll('[data-animação]')
 const animação_classe = 'animação'
 
 function animação_scroll() {
-    const topo_página_janela = window.pageYOffset((window.innerHeight * 3) / 4) //Três quartos da janela como referência.
+    const topo_página_janela = window.pageYOffset + ((window.innerHeight * 3) / 4) //Três quartos da janela como referência.
     //Definindo de onde a animação vai partir. Quanto mais se somar a esse comando, mais embaixo a animação será iniciada.
 
     elementos.forEach(function(elemento) { //Cada item de "elementos" será repassado para "elemento".
