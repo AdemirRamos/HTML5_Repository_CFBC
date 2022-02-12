9332bd9d6260cb1abf32efef9246719f38065a27
AdemirRamos <ademirramos634@gmail.com>
2022-02-12 14:50:47 -0300

    Adição da Aula 92

---
+++ CFB Cursos/Aula 92/aula92.js
@@ -0,0 +1,49 @@
+//As versões mais recentes do JS recomendam que, caso uma variável seja destinada a guarda elementos, ela deve ser "const".
+
+//Seja em JS ou CSS, os colchetes servem para selecionar parâmetros.
+
+const elementos = document.querySelectorAll('[data-animação]')
+
+const animação_classe = 'animação'
+
+function animação_scroll() {
+    const topo_página_janela = window.pageYOffset + ((window.innerHeight * 3) / 4) //Três quartos da janela como referência.
+    //Definindo de onde a animação vai partir. Quanto mais se somar a esse comando, mais embaixo a animação será iniciada.
+
+    elementos.forEach(function(elemento) { //Cada item de "elementos" será repassado para "elemento".
+        if (topo_página_janela > elemento.offsetTop) {
+            elemento.classList.add(animação_classe)
+        }
+
+        else {
+            elemento.classList.remove(animação_classe)
+        }
+    })
+}
+
+//Verificando se os elementos da página, eventualmente, precisarão receber a animação:
+
+if (elementos.length) { //0 == false; 1 == true.
+    window.addEventListener('scoll', function() {
+        animação_scroll()
+    })
+}
+
+function clique_modal(img) {
+    const janela_modal = document.getElementById('janela_modal')
+    const imagem_modal = document.getElementById('imagem_modal')
+    const texto_modal = document.getElementById('texto_modal')
+    const botão_fechar = document.getElementById('botão_fechar')
+
+    janela_modal.classList.remove('ocultar_janela_modal') //Conferir "classList".
+    janela_modal.classList.add('mostrar_janela_modal')
+
+    imagem_modal.src = img.src //É capturado o "src" do elemento passado como parâmetro da função.
+    imagem_modal.alt = img.alt //O "alt" da imagem passada como parâmetro é capturado.
+    imagem_modal.innerHTML = img.alt //O conteúdo de "alt" será exibido na tela.
+
+    botão_fechar.onclick = function() {
+    janela_modal.classList.add('ocultar_janela_modal')
+    janela_modal.classList.remove('mostrar_janela_modal')
+    }
+}
