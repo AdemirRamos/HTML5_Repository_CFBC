4e4f2a5cace4dbd8a70088e43f81dd959a12b90d
AdemirRamos <ademirramos634@gmail.com>
2022-03-06 14:40:33 -0300

    Adição da Aula 124

---
+++ CFB Cursos/Aula 124/aula124.js
@@ -0,0 +1,16 @@
+var i = 0
+var texto = 'CFB Cursos - Curso de HTML Completo e Profissional - www.youtube.com/cfbcursos'
+var velocidade = 150 //Aumente o valor para diminuir a velocidade; diminua o valor para aumentar a velocidade.
+
+function digitação() {
+    if (i < texto.length) {
+        document.getElementById('texto').innerHTML += texto.charAt(i)
+        i++
+
+        //Alterando o intervalo de "STO":
+
+        //velocidade = Math.round(Math.random()*150)
+
+        setTimeout(digitação, velocidade)
+    }
+}
