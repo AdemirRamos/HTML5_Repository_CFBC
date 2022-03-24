08949355b335ef7d33842c82d8da74b9c4b00ae1
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-23 22:50:50 -0300

    Atualização da Aula 95
    Apenas foram adicionadas explicações sobre alguns elementos usados no documento.

--- CFB Cursos/Aula 95/aula95.js
+++ CFB Cursos/Aula 95/aula95.js
@@ -4,6 +4,9 @@ for (let i = 0; i < acordeon_array.length; i++) {
     acordeon_array[i].addEventListener('click', function() {
         this.classList.toggle('selecionado') //Checar "toggle".
         const painel = this.nextElementSibling //Checar.
+        
+        "nextElementSibling": https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling
+        "this": https://desenvolvimentoparaweb.com/javascript/this-javascript-dominando/
 
         if (painel.style.maxHeight) {
             painel.style.maxHeight = null
