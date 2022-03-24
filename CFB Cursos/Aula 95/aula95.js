d62c7a16f5473639a60eb5031d0fc6feec91ca99
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-23 22:52:54 -0300

    Atualização da Aula 95 (aula95.js)
    Apenas foram removidas, do documento, algumas mensagens excedentes.

--- CFB Cursos/Aula 95/aula95.js
+++ CFB Cursos/Aula 95/aula95.js
@@ -2,8 +2,8 @@ const acordeon_array = document.getElementsByClassName('acordeon')
 
 for (let i = 0; i < acordeon_array.length; i++) {
     acordeon_array[i].addEventListener('click', function() {
-        this.classList.toggle('selecionado') //Checar "toggle".
-        const painel = this.nextElementSibling //Checar.
+        this.classList.toggle('selecionado')
+        const painel = this.nextElementSibling
         
         "nextElementSibling": https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling
         "this": https://desenvolvimentoparaweb.com/javascript/this-javascript-dominando/
