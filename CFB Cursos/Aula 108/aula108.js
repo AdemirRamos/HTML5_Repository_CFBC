559f6c6171b3edd9eead4c0f7954452e5d690204
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-26 17:16:31 -0300

    Atualização da Aula 108 (aula108.js)
    Apenas fora adicionado um "link" com uma explicação para "parentElement", na 6ª linha do documento.

--- CFB Cursos/Aula 108/aula108.js
+++ CFB Cursos/Aula 108/aula108.js
@@ -1,5 +1,7 @@
 function mostrar_imagem(imagem) {
     const imagem_grande = document.getElementById('imagem_grande')
     imagem_grande.src = imagem.src
-    imagem_grande.parentElement.style.display = 'block' //Checar.
+    imagem_grande.parentElement.style.display = 'block'
+    
+    //"parentElement": https://www.w3schools.com/jsref/prop_node_parentelement.asp
 }
