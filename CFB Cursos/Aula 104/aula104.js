2f0b996204b33edd031b7d031382c14c939c1738
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-26 15:54:33 -0300

    Atualização da Aula 104 (aula104.js)
    Foram adicionados, ao longo do documento, "links" contendo explicações sobre elementos usados no documento.

--- CFB Cursos/Aula 104/aula104.js
+++ CFB Cursos/Aula 104/aula104.js
@@ -2,15 +2,20 @@ function mostrar_menu() {
     document.getElementsByClassName('dropdown_conteúdo')[0].classList.toggle('mostrar_menu')
 }
 
-window.onclick = function(event) { //Checar.
-    if (!event.target.matches('.botão_dropdown')) { //Checar.
+window.onclick = function(event) {
+    if (!event.target.matches('.botão_dropdown')) {
         let dropdown = document.getElementsByClassName('dropdown_conteúdo')
-
+        
+        //"target": https://www.w3schools.com/jsref/event_target.asp
+        //"matches": https://www.w3schools.com/jsref/met_element_matches.asp
+        
         for (let i = 0; i < dropdown.length; i++) {
             let abrir_dropdown = dropdown[i]
 
-            if (abrir_dropdown.classList.contains('mostrar_menu')) { //Checar.
+            if (abrir_dropdown.classList.contains('mostrar_menu')) {
                 abrir_dropdown.classList.remove('mostrar_menu')
+                
+            //"contain": https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/includes
             }
         }
     }
