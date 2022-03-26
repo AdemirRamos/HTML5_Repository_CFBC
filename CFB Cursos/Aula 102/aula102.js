97e000ba489c131727dbd24d8d1daa3570ad5c30
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-25 22:42:53 -0300

    Atualização da Aula 102 (aula102.js)
    Foram adicionados, ao documento, "links" com explicações acerca dos elementos usados no mesmo.

--- CFB Cursos/Aula 102/aula102.js
+++ CFB Cursos/Aula 102/aula102.js
@@ -1,12 +1,12 @@
 const nav = document.getElementsByTagName('nav')[0]
 const topo_nav = nav.offsetTop
 
-"offsetTop": https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLElement/offsetTop
+//"offsetTop": https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLElement/offsetTop
 
 window.onscroll = function() {
     fixar_menu_no_topo()
 
-    
+    //Ao se usar uma função anônima aqui, podemos chamar várias funções de uma só vez.
 }
 
 //"offSetTop" é um método que retorna a medida (em pixels) da distância de um elemento em relação ao topo do elemento-pai (anterior).
@@ -16,7 +16,7 @@ function fixar_menu_no_topo() {
         nav.classList.add('fixo_no_topo')
     }
     
-    
+    //"pageYOffSet": https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
 
     else {
         nav.classList.remove('fixo_no_topo')
