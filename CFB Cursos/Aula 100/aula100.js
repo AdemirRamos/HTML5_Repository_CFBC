7c6225f3cc1fe53036ebed863f6b133c52057d69
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-25 21:40:22 -0300

    Atualização da Aula 100 (aula100.js)
    Apenas foram adicionados "links" com informações sobre alguns elementos aplicados à página. - Linhas de inserção da documentação: 11ª e 12ª linhas.

--- CFB Cursos/Aula 100/aula100.js
+++ CFB Cursos/Aula 100/aula100.js
@@ -3,10 +3,13 @@ window.onscroll = function() {
 }
 
 function rolar() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) { //Checar.
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementsByTagName('header')[0].style.padding = '30px 10px'
         document.getElementById('logo').style.fontSize = '25px'
     }
+    
+    "scrollTop": https://developer.mozilla.org/pt-BR/docs/Web/API/Element/scrollTop
+    "documentElement": https://developer.mozilla.org/pt-BR/docs/Web/API/Document/documentElement
 
     else {
         document.getElementsByTagName('header')[0].style.padding = '80px 10px'
