54894193b5fa25065eb38199b8b35f4078cb1b1a
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-26 20:33:57 -0300

    Atualização da Aula 119 (aula119.js)
    Fora removida, da 5ª linha do documento, uma anotação lembrando-me para pesquisar sobre "getModifierState".

--- CFB Cursos/Aula 119/aula119.js
+++ CFB Cursos/Aula 119/aula119.js
@@ -2,7 +2,7 @@ var caixa_texto = document.getElementById('caixa_de_texto')
 var texto = document.getElementById('texto_alerta')
 
 caixa_texto.addEventListener('keyup', function(event) {
-    if (event.getModifierState('CapsLock')) { //Checar.
+    if (event.getModifierState('CapsLock')) {
         texto.style.display = 'block'
         
     //"getModifierState": https://www.w3schools.com/jsref/event_key_getmodifierstate.asp
