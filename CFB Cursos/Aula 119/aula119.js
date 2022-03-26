f25d95c31cbba671089f68374388ac464ce5dfac
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-26 20:30:13 -0300

    Atualização da Aula 119 (aula119.js)
    À 6ª linha do documento, fora adicionado um "link" contendo uma explicação para o "getModifierState".

--- CFB Cursos/Aula 119/aula119.js
+++ CFB Cursos/Aula 119/aula119.js
@@ -4,6 +4,8 @@ var texto = document.getElementById('texto_alerta')
 caixa_texto.addEventListener('keyup', function(event) {
     if (event.getModifierState('CapsLock')) { //Checar.
         texto.style.display = 'block'
+        
+        //"getModifierState": https://www.w3schools.com/jsref/event_key_getmodifierstate.asp
     }
 
     else {
