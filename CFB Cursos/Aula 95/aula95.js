b05ce904767140e8c60d0a01250a2cd49a48f515
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-04-15 18:36:38 -0300

    Atualização da Aula 95 (aula95.js)
    Os "links" com informações foram transformados em comentários.

--- CFB Cursos/Aula 95/aula95.js
+++ CFB Cursos/Aula 95/aula95.js
@@ -5,8 +5,8 @@ for (let i = 0; i < acordeon_array.length; i++) {
         this.classList.toggle('selecionado')
         const painel = this.nextElementSibling
         
-        "nextElementSibling": https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling
-        "this": https://desenvolvimentoparaweb.com/javascript/this-javascript-dominando/
+        /*"nextElementSibling": https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling*/
+        /*"this": https://desenvolvimentoparaweb.com/javascript/this-javascript-dominando/*/
 
         if (painel.style.maxHeight) {
             painel.style.maxHeight = null
@@ -16,7 +16,7 @@ for (let i = 0; i < acordeon_array.length; i++) {
             painel.style.maxHeight = painel.scrollHeight + 'px'
         }
         
-        "scrollHeight": https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight
-        "maxHeight": https://www.w3schools.com/cssref/pr_dim_max-height.asp
+        /*"scrollHeight": https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight*/
+        /*"maxHeight": https://www.w3schools.com/cssref/pr_dim_max-height.asp*/
     })
 }
